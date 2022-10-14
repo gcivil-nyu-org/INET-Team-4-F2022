70084bb0298676c1d509b0da479b90af604a87f7
Fayed Gaya <gayafayed@gmail.com>
2022-10-14 12:41:00 -0400

    Add login, logout, navbar

--- BeHonest/main/urls.py
+++ BeHonest/main/urls.py
@@ -6,5 +6,7 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    path("register", views.register_request, name="register")
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name= "logout"),
 ]
--- BeHonest/main/views.py
+++ BeHonest/main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import NewUserForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 # Main views
 def homepage(request):
@@ -18,3 +19,26 @@ def register_request(request):
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
 	return render (request=request, template_name="main/register.html", context={"register_form":form})
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("main:homepage")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="main/login.html", context={"login_form":form})
+
+def logout_request(request):
+	logout(request)
+	messages.info(request, "You have successfully logged out.") 
+	return redirect("main:homepage")
