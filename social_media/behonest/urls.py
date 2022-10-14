f5ed5a5d521dbacb2d59b3bb7d85fcdfeea9ea40
Fayed Gaya <gayafayed@gmail.com>
2022-10-14 10:00:57 -0400

    Add registration page

--- social_media/behonest/urls.py
+++ social_media/behonest/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
 
+app_name = "behonest"
+
 urlpatterns = [
-    path('', views.register, name='detail')
+    path("register/", views.register_request, name='register')
 ]
--- social_media/behonest/views.py
+++ social_media/behonest/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login 
+from django.contrib import messages
 from django.http import HttpResponse
 from django.template import loader
 
@@ -6,8 +9,14 @@ from django.template import loader
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def register(request):
-    template = loader.get_template('behonest/register.html')
-    return render(request, 'behonest/register.html')
-    
-
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("main:homepage")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request=request, template_name="main/register.html", context={"register_form":form})
