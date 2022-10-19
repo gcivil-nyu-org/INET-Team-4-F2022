7aa288c5ba35591613a30f31d339e17bde72771f
kevin <wwwkevin12@163.com>
2022-10-19 10:24:33 -0400

    blog html fixed

--- BeHonest/blog/urls.py
+++ BeHonest/blog/urls.py
@@ -1,7 +1,8 @@
 from . import views
 from django.urls import path
 
+app_name = 'blog'
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('', views.PostList.as_view(), name='base'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
