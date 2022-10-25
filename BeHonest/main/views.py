22467b117b1008d12def45cf2a4571915cd53aed
h2752 <hh2752@nyu.edu>
2022-10-24 21:02:24 -0400

    Login and Post Mergerd

--- BeHonest/main/views.py
+++ BeHonest/main/views.py
@@ -36,6 +36,7 @@ def login_request(request):
         else:
             messages.error(request,"Invalid username or password.")
     form = AuthenticationForm()
+
     return render(request=request, template_name="main/login.html", context={"login_form":form})
 
 def logout_request(request):
