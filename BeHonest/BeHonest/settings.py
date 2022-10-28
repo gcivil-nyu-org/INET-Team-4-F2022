f156c9e90e9fdf4651f0e3b75b1c964982c1316c
Fayed Gaya <gayafayed@gmail.com>
2022-10-28 05:40:31 -0400

    Travis pip installs from requriements.txt, implement flake8 suggested changes

--- BeHonest/BeHonest/settings.py
+++ BeHonest/BeHonest/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
+# import os
 from django.contrib.messages import constants as messages
 from pathlib import Path
 
