0a4f5ea42f65fa4f4e3ca49ad8dcb84c2d25673d
Wenkai Cao <44592494+kevin949373048@users.noreply.github.com>
2022-10-28 17:20:31 -0400

    add news

--- BeHonest/main/urls.py
+++ BeHonest/main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from news.views import index
 from post.views import post_list
 
 app_name = "main"
@@ -8,6 +9,7 @@ app_name = "main"
 urlpatterns = [
     path("home", post_list, name="base"),
     path("register", views.register_request, name="register"),
+    path('news', index, name="news"),
     path("login/", views.login_request, name="login"),
     path("logout", views.logout_request, name="logout"),
     path("", views.homepage, name="homepage"),
