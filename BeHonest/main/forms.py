a162ac0f9a7f668260f4dc4dda321e033a3b251b
Fayed Gaya <gayafayed@gmail.com>
2022-10-28 05:28:11 -0400

    Fix Flake8 reccomendation for forms.py

--- BeHonest/main/forms.py
+++ BeHonest/main/forms.py
@@ -9,12 +9,12 @@ class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
-	    model = User
-	    fields = ("username", "email", "password1", "password2")
+        model = User
+        fields = ("username", "email", "password1", "password2")
 
     def save(self, commit=True):
-	    user = super(NewUserForm, self).save(commit=False)
-	    user.email = self.cleaned_data['email']
-	    if commit:
-		    user.save()
-	    return user
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+            return user
