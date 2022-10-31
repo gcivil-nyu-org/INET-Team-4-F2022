4f0e0756433f659cf84f595a3aeb43895e182cb2
wheins7 <71990486+wheins7@users.noreply.github.com>
2022-10-31 13:54:50 -0400

    Test Works

--- BeHonest/post/views.py
+++ BeHonest/post/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render,redirect
 from django.urls import reverse
 from django.contrib.auth import logout
 from django.contrib import messages
