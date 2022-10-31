a42a3a99999db4de215330f8df04667cba6725b1
Kil-Hwan <kilhwanpyun@gmail.com>
2022-10-31 03:48:07 -0400

    changed title and redirects

--- BeHonest/main/urls.py
+++ BeHonest/main/urls.py
@@ -7,7 +7,7 @@ app_name = "main"
 
 urlpatterns = [
     path("home", post_list, name="base"),
-    path("register", views.register_request, name="register"),
+    path("register/", views.register_request, name="register"),
     path("login/", views.login_request, name="login"),
     path("logout", views.logout_request, name="logout"),
     path("", views.homepage, name="homepage"),
