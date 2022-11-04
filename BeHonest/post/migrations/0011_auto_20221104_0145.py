1c2c5c1d67a07c8b6dc189d00fbfac016a3ff7e7
Kil-Hwan <kilhwanpyun@gmail.com>
2022-11-03 22:18:09 -0400

    flake8 and black

--- BeHonest/post/migrations/0011_auto_20221104_0145.py
+++ BeHonest/post/migrations/0011_auto_20221104_0145.py
@@ -6,18 +6,22 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('post', '0010_post_dislikes_alter_comment_id_alter_post_id'),
+        ("post", "0010_post_dislikes_alter_comment_id_alter_post_id"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='comment',
-            name='id',
-            field=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="comment",
+            name="id",
+            field=models.AutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
         ),
         migrations.AlterField(
-            model_name='post',
-            name='id',
-            field=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="post",
+            name="id",
+            field=models.AutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
         ),
     ]
