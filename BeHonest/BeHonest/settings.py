2adb02f91205e1eda6cdd5669939ba4b92fdb6e5
Wenkai Cao <44592494+kevin949373048@users.noreply.github.com>
2022-11-07 16:51:02 -0500

    Update settings.py

--- BeHonest/BeHonest/settings.py
+++ BeHonest/BeHonest/settings.py
@@ -139,4 +139,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
